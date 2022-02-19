c39838b67a3a25aa5f62df2ce2180a3f1e44ee72
Shilu Tudu <shilutudu123@gmail.com>
2022-02-19 20:20:04 +0530

    keep

--- Hangman.pl
+++ Hangman.pl
@@ -1,6 +1,8 @@
 #!/usr/local/bin/perl
 use strict;
 
+my @guess_arr;
+
 sub getrandom_word {
     my $arr = @_[0];
     my $sz = @_[1];
@@ -12,8 +14,22 @@ sub getrandom_word {
 sub prnt {
     my $wrd = $_[0];
     foreach my $char (split //, $wrd) {
-        print "_ ";
+        # print "_ ";
+        print uc $char;
     }
+}
+
+sub check {
+    my $wrd = $_[0];
+    my $guess = $_[1];
+    print (ord(uc $guess) - 65),"\n";
+    # @guess_arr[] 
+    foreach my $char (split //, $wrd) {
+        if($char eq $guess) {
+            return 1;
+        }
+    }
+    return 0;
 }
 
 my $filename = ($ARGV[0]);
@@ -33,7 +49,11 @@ close $fh or die "Couldn't Close File : $filename";
 my $size = @data;
 my $word = getrandom_word(\@data, $size);
 my $remain = 6;
-my $guess;
+my $guess=='';
+
+for(my $i = 0; $i < 25; $i++) {
+    @guess_arr[$i] = 0;
+}
 
 print "Welcome player this is a Hangman Game.\n";
 while($remain) {
@@ -42,5 +62,16 @@ while($remain) {
     print "\nGuesses so far : \n";
     print "Make a Guess : ";
     $guess = <STDIN>;
+    chop($guess);
+    push(@guess_arr, $guess);
+    # $hs{$guess} = 1;
+    if(check($word, $guess)) {
+        print "Present\n";
+    }
+    else {
+        print "Not present\n";
+        $remain--;
+    }
     print "You have $remain body parts left\n";
 }
+
