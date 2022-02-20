d0c64bd2f7c1677be133ce80ae026f1d54cbc99f
Shilu Tudu <shilutudu123@gmail.com>
2022-02-20 13:22:05 +0530

    keep3

--- Game.pl
+++ Game.pl
@@ -4,6 +4,8 @@ use strict;
 use warnings;
 
 my @alpha_hash = ();
+my $isgameover;
+
 my @word_data = (
     "computer", "radio", "calculator", "teacher", "bureau", "police", "geometry", "president", "subject", "country", 
     "enviroment", "classroom", "animals", "province", "month", "politics", "puzzle", "instrument", "kitchen", "language", 
@@ -42,38 +44,98 @@ sub Print_word {
 sub body {
     my ($cnt) = @_;
 
-    print "   _______\n";
-    print "  |       |\n";
+    print "\t   _______\n";
+    print "\t  |       |\n";
     for(my $i = 0; $i < 5; $i++) {
         my $str;
-        $str = (($cnt >= 1 && $i == 0)?              "  |       O\n":"  |\n");
-        $str = (($cnt >= 2 && ($i == 1 || $i == 2))? "  |       |     \n": $str);
-        $str = (($cnt >= 3 && ($i == 1))?             "  |      \\|   \n": $str);
-        $str = (($cnt >= 4 && ($i == 1))?             "  |      \\|\/ \n": $str);
-        $str = (($cnt >= 5 && ($i == 3))?             "  |      \/    \n": $str);
-        $str = (($cnt >= 6 && ($i == 3))?             "  |      \/ \\ \n": $str);
+        $str = (($cnt >= 1 && $i == 0)?              "\t  |       O\n":"\t  |\n");
+        $str = (($cnt >= 2 && ($i == 1 || $i == 2))? "\t  |       |     \n": $str);
+        $str = (($cnt >= 3 && ($i == 1))?             "\t  |      \\|   \n": $str);
+        $str = (($cnt >= 4 && ($i == 1))?             "\t  |      \\|\/ \n": $str);
+        $str = (($cnt >= 5 && ($i == 3))?             "\t  |      \/    \n": $str);
+        $str = (($cnt >= 6 && ($i == 3))?             "\t  |      \/ \\ \n": $str);
         print $str;
     }
-    print "__|__\n\n\n";
+    print "\t__|__\n\n";
+}
+
+sub isalready_guessed {
+    my ($guess_arr, $guess) = @_;
+
+    foreach my $x (@$guess_arr) {
+        if((uc $x) eq (uc $guess)) {
+            print "Sorry you already guessed $guess\n";
+            return 1;
+        }
+    }
+    return 0;
+}
+
+sub update {
+    my ($guess) = @_;
+
+    my $idx = (ord(uc $guess) - 65);
+    $alpha_hash[$idx] = 1;
+}
+
+sub check {
+    my ($guess, $word, $lft_parts) = @_;
+
+    my $i = 0;
+    foreach my $x (split //, $word) {
+        if( $guess eq $x) {
+            $i++;
+        }
+    }
+    if($i == 0) {
+        ($$lft_parts)--;
+        print "Bad guess!!, you are left with ($$lft_parts) body parts\n";
+    }
+    else {
+        print "Good guess!!, you are left with ($$lft_parts) body parts\n";
+    }
+}
+
+sub Game_start {
+    my ($word) = @_;
+
+    my $lft_parts = 6;
+    my $guess; 
+    my @guess_arr;
+    while ($lft_parts) {
+        body(6 - $lft_parts);
+        print "Here is the word : ";
+        Print_word($word);
+        print "\nGuesses so far : ";
+        print join(', ', @guess_arr), "\n";
+        print "Pick an alphabet : ";
+        $guess = <STDIN>;
+        chomp($guess);
+        while(isalready_guessed(\@guess_arr, $guess)) {
+            print "\nPick an alphabet : ";
+            $guess = <STDIN>;
+            chomp($guess);
+        }
+        push(@guess_arr, $guess);
+        update($guess);
+        check($guess, $word, \$lft_parts);
+    }
 }
 
 
 sub int_main {
     # print gen_random_word(), "\n";
 
+    $isgameover = 0;
+    
     for(my $i = 0; $i < 26; $i++) {
         $alpha_hash[$i] = 0;
     }
     my $word = gen_random_word();
+    print $word, "\n";
     print "Welcome player, this is a Hangman Game\n";
-    print "Here is your word : ";
-    Print_word($word);
-    for ( my $i = 1; $i <= 6; $i++) {
-        body($i);
-    }
+    Game_start($word);
 }
-
-
 
 
 int_main();
